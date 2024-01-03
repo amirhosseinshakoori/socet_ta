40390b9d06ea7cd7730d2f2f3898d53bd7be04ba
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2024-01-03 15:18:08 +0330

    finish

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import socket
 
 
 def start_client(server_address, port):
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_address, port))
 
