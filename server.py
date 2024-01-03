d4f521b9ccd022d6d32ac6ca664aaf72f37cf549
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2024-01-03 12:47:33 +0330

    define client_handler for handel client sata

--- server.py
+++ server.py
@@ -2,13 +2,29 @@ import socket
 import threading
 
 
+def client_handler(client_socket):
+    while True:
+        
+        encoded_data = client_socket.recv(1024)
+        if not encoded_data:
+            break
 
+        
+        decoded_data = encoded_data.decode('ascii')
+
+        
+        client_socket.send(decoded_data.encode('ascii'))
+
+    client_socket.close()
 
 def start_server(address, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((address, port))
     server_socket.listen(5)
     print(f"[*] Listening on {address}:{port}")
+
+
+
 
 
 if __name__ == '__main__':
