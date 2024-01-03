033cd6ac98e27cf8c26405d3375baed1a9412fc2
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2024-01-03 12:35:08 +0330

    start_server function is define

--- server.py
+++ server.py
@@ -4,7 +4,11 @@ import threading
 
 
 
-
+def start_server(address, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((address, port))
+    server_socket.listen(5)
+    print(f"[*] Listening on {address}:{port}")
 
 
 if __name__ == '__main__':
