46d2ee16b1394440f349f9ef625c6e478b800927
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2024-01-03 15:03:36 +0330

    start_client define

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
 import socket
+
+
+
+def start_client(server_address, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_address, port)
+
+
+
+if __name__ == '__main__':
+    HOST = 'localhost'
+    PORT = 12345
+    start_client(HOST, PORT)
